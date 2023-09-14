e36aff2f8726b57054da021d56d59c645a27a7a4
Dilshan Fernando <dilshantrx@gmail.com>
2023-09-14 21:59:46 +0530

    Initial commit

---
+++ src/test/java/com/hr_api/Hr_Management_System/HrManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr_api.Hr_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
